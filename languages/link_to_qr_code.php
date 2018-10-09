f3bfa30875bed16061c74714c2a0edc0ddf1a441
dinislambds <din2243@gmail.com>
2018-10-09 21:11:45 +0600

    encode url change

--- languages/link_to_qr_code.php
+++ languages/link_to_qr_code.php
@@ -27,7 +27,7 @@ function link_to_qr_callback( $content ){
     // Current Post ID
     $post_id = get_the_ID();
     $post_title = get_the_title( $post_id );
-    $post_url = urlencode(get_the_permalink( $post_id ));
+    $post_url = rlencode(get_the_permalink( $post_id ));
     $image_source = sprintf( "https://api.qrserver.com/v1/create-qr-code/?size=185x185&ecc=L&qzone=1&data=%s", $post_url );
     $content .= sprintf( "<div class='qrcode_img'><img src='%s' alt='%s'></div>", $image_source, $post_title );
 
